d906307a126d4b06b85d8f2faab7d7f5c11093dd
codeByAfzal <afzalxcode@gmail.com>
2025-09-09 22:53:02 +0530

    Counts copy to clipboard

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const sentencesResult = document.getElementById('sentence-span');
 const paragraphsResult = document.getElementById('paragraph-span');
 const spacesResult = document.getElementById('spaces-span');
 const result = document.querySelectorAll('.result-div');
+const characterDiv = document.getElementById('character-div');
+const wordDiv = document.getElementById('word-div');
+const sentenceDiv = document.getElementById('sentence-div');
+const paraDiv = document.getElementById('paragraph-div');
+const spaceDiv = document.getElementById('spaces-div');
 
 
 typingArea.addEventListener('input', function() {
@@ -82,3 +87,43 @@ function spacesCount() {
     }
 
 }
+
+// Characters Count Copy
+characterDiv.addEventListener('click', function() {
+    
+    navigator.clipboard.writeText(charactersResult.textContent);
+    alert("Characters Count Copied!");
+
+})
+
+// Words Count Copy
+wordDiv.addEventListener('click', function() {
+    
+    navigator.clipboard.writeText(wordsResult.textContent);
+    alert("Words Count Copied!");
+
+})
+
+// Sentence Count Copy
+sentenceDiv.addEventListener('click', function() {
+    
+    navigator.clipboard.writeText(sentencesResult.textContent);
+    alert("Sentence Count Copied!");
+
+})
+
+// Paragraph Count Copy
+paraDiv.addEventListener('click', function() {
+    
+    navigator.clipboard.writeText(paragraphsResult.textContent);
+    alert("Paragraph Count Copied!");
+
+})
+
+// Spaces Count Copy
+spaceDiv.addEventListener('click', function() {
+    
+    navigator.clipboard.writeText(spacesResult.textContent);
+    alert("Spaces Count Copied!");
+
+})
